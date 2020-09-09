9494e7460ec11e4b4a998ba7bdd8df6944101630
chetan dehane <62801180+ChetanDehane@users.noreply.github.com>
2020-09-10 01:59:36 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# hello
+# i my name is gaurav . i always wanted to create a website so i learned html,css,javascript etc. so this is my website
---
+++ js/babel.js
@@ -0,0 +1,22 @@
+
+// eslint-disable-next-line no-unused-vars
+const projectName = 'portfolio';
+
+var $button = $('#myButton'),
+  $text   = $('#myText'),
+  visible = true;
+
+$button.click(function(){
+if ( visible ) {
+  $text.slideUp('fast',function(){
+    $text.addClass('hide')
+         .slideDown(0);
+  });
+} else {
+  $text.slideUp(0,function(){
+    $text.removeClass('hide')
+         .slideDown('fast');
+  });
+}
+visible = ! visible;
+});
