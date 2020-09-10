0b51a459434d55a12f98f4c5b01278e1f012001b
chetan dehane <62801180+ChetanDehane@users.noreply.github.com>
2020-09-10 17:30:00 +0530

    version-1.0
    fresh view

--- js/java.js
+++ js/java.js
@@ -1,8 +1,8 @@
 function myFunction() {
   var x = document.getElementById('myDIV');
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
+  if (x.style.display == "block") {
     x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
