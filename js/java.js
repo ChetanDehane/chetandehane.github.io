051e8f889ebb30141d8de5a587611fc8ac49978f
chetan dehane <62801180+ChetanDehane@users.noreply.github.com>
2020-10-28 01:20:42 +0530

    dark mode in microcontroller project

--- js/java.js
+++ js/java.js
@@ -1,8 +1,27 @@
-function myFunction() {
+function moreaboutme() {
   var x = document.getElementById('myDIV');
   if (x.style.display == "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
+  }
+}
+
+
+function darkmode() {
+
+if(document.body.style.backgroundColor == "white")
+{
+  document.getElementById("darkmodebutton").childNodes[0].nodeValue=
+   "Normal Mode";
+  document.body.style.backgroundColor = "#000000cf";
+  document.body.style.color = "white";
+  }
+  else
+  {
+  	document.getElementById("darkmodebutton").childNodes[0].nodeValue=
+   "Dark Mode";
+  document.body.style.backgroundColor = "white";
+  document.body.style.color = "#000000cf";
   }
 }
