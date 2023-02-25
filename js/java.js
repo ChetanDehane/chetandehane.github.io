c53cf003246bea1138516c99d9b87814dc467394
ChetanDehane <chetanpdehane@gmail.com>
2023-02-25 18:22:41 +0530

    Website Updated - Version 2
    1) Added more responsiveness.
    2) change navbar.
    3) change project section.
    and many more...

--- js/java.js
+++ js/java.js
@@ -1,39 +1,42 @@
-function moreaboutme() {
-  var x = document.getElementById('myDIV');
-  var y = document.getElementById('welcome-section');
-  if (x.style.display == "block") {
-    x.style.display = "none";
-    y.style.height = "90rem";
-
-  } else {
-    x.style.display = "block";
-    y.style.height = "140rem";
-  }
+window.onload = function() {
+  window.scrollTo(0, 0);
 }
 
 
 function darkmode() {
+
+  var link1 = document.querySelectorAll(".links")[0];
+  var link2 = document.querySelectorAll(".links")[1];
 
   if (document.body.style.backgroundColor == "white") {
     document.getElementById("darkmodebutton").childNodes[0].nodeValue =
       "Normal Mode";
     document.body.style.backgroundColor = "#000000cf";
     document.body.style.color = "white";
+
+    
+    if(link1 || link2) {
+      link1.style.color = "#ddd";
+      link1.style.textDecoration.none;
+
+      link2.style.color = "#ddd";
+      link2.style.textDecoration.none;
+    }
+    
   }
   else {
     document.getElementById("darkmodebutton").childNodes[0].nodeValue =
       "Dark Mode";
     document.body.style.backgroundColor = "white";
     document.body.style.color = "#000000cf";
+
+    if(link1 || link2) {
+      link1.style.color = "blue";
+      link1.style.textDecoration.initial;
+
+      link2.style.color = "blue";
+      link2.style.textDecoration.initial;
+    }
+
   }
 }
-
-
-/*function navFunction() {
-  var x = document.getElementById("nav-ele");
-  if (x.className === "nav-list") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav-list";
-  }
-}*/
